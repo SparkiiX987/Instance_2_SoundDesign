94f42a241d579f6a3d016121affbef3a07000ca1
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2026-03-16 14:04:35 +0100

    add switch and door

---
+++ Assets/Obstacles/Scripts/Interface/IInteracable.cs
@@ -0,0 +1,4 @@
+public interface IInteracable
+{
+    public void Interact();
+}
---
+++ Assets/Utils/Events/Events.cs
@@ -0,0 +1,9 @@
+public struct OnSwitchOnEvent
+{
+    public int switchId;
+}
+
+public struct OnSwitchOffEvent
+{
+    public int switchId;
+}
