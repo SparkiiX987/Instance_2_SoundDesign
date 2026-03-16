3996a9e523bf83ff177fa02215cf3c30875ec8db
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2026-03-16 15:36:16 +0100

    add debugStartGame menu

---
+++ Assets/Game/Scripts/MenuDebug.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuDebug : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
