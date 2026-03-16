e9c4a5419e073f8ccc5ec0e2dfc8166a93cf6923
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2026-03-16 15:15:49 +0100

    add GameManager

---
+++ Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Transform playerSpawnPoint;
+
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        EventBus.Subscribe<OnTrapEnter>(KillPlayer);
+        EventBus.Subscribe<OnDefaite>(Defaite);
+        EventBus.Subscribe<OnVictory>(Victory);
+
+        Instantiate(playerPrefab, playerSpawnPoint.position, Quaternion.identity);
+    }
+
+    private void OnDestroy()
+    {
+        EventBus.Unsubscribe<OnTrapEnter>(KillPlayer);
+        EventBus.Unsubscribe<OnDefaite>(Defaite);
+        EventBus.Unsubscribe<OnVictory>(Victory);
+    }
+
+    private void KillPlayer(OnTrapEnter enter)
+    {
+        print("player killed");
+
+        EventBus.Publish(new OnDefaite());
+    }
+
+    private void Defaite(OnDefaite defaite)
+    {
+        print("defaite");
+    }
+
+    private void Victory(OnVictory victory)
+    {
+        print("victory");
+    }
+}
